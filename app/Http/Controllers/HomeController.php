75877dc6527ba8063aa4f1d4a94c30f5a17fc2f5
Wsjls <418383219@qq.com>
2022-02-10 00:16:58 +0800

    中间件

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Middleware\Benchmark;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller{
+
+    public function __construct()
+    {
+//        $this->middleware(Benchmark::class);
+//        或者
+        $this->middleware('benchmark:test1,test2', ['only' => ['hello']]);
+        // 方便做管理
+        // 指定函数排除 ['except' => ['hello']])
+        // 指定生效 ['only' => ['hello']])
+//        做鉴权
+//        $this->middleware('auth:admin,general', ['only' => ['hello']]);
+    }
+
     public function hello()
     {
         return "hello 1";
+    }
+
+    public function hello2()
+    {
+        return "hello 2";
     }
 
     public function getOrder(Request $request, $id=10, $name) {
